3a21c0f17f07607b42236e3fec7ca63a90493c68
Benjamin Ebert <bebert410@googlemail.com>
2021-12-31 14:41:56 +0100

    Add comments to Follow crud operations

--- database/follow.go
+++ database/follow.go
@@ -6,18 +6,25 @@ import (
 	"wtfTwitter/errs"
 )
 
+// FollowService manages Follows.
+// It implements the domain.FollowService interface.
 type FollowService struct {
 	followValidator
 }
 
+// followValidator runs validations on incoming Follow data.
+// On success, it passes the data on to followGorm. Otherwise, it returns an error.
 type followValidator struct {
 	followGorm
 }
 
+// followGorm runs CRUD operations on the database using incoming Follow data.
+// It assumes that data has been validated. On success, it returns nil. Otherwise, it returns an error.
 type followGorm struct {
 	db *gorm.DB
 }
 
+// NewFollowService returns an instance of FollowService.
 func NewFollowService(db *gorm.DB) *FollowService {
 	return &FollowService{
 		followValidator{
@@ -28,8 +35,10 @@ func NewFollowService(db *gorm.DB) *FollowService {
 	}
 }
 
+// This checks if the FollowService struct properly implements the domain.FollowService interface.
 var _ domain.FollowService = &FollowService{}
 
+// Create runs validations needed before creating new Follow database records.
 func (fv *followValidator) Create(follow *domain.Follow) error {
 	err := runFollowValFns(follow,
 		fv.followedUserExists,
@@ -41,6 +50,7 @@ func (fv *followValidator) Create(follow *domain.Follow) error {
 	return fv.followGorm.Create(follow)
 }
 
+// Delete runs validations needed before deleting existing Follow database records.
 func (fv *followValidator) Delete(follow *domain.Follow) error {
 	err := runFollowValFns(follow, fv.followExists)
 	if err != nil {
@@ -49,6 +59,8 @@ func (fv *followValidator) Delete(follow *domain.Follow) error {
 	return fv.followGorm.Delete(follow)
 }
 
+// runFollowValFns runs any number of functions of type followValFn on the passed in Follow object.
+// If none of them returns an error, it returns nil. Otherwise, it returns the respective error.
 func runFollowValFns(follow *domain.Follow, fns ...followValFn) error {
 	for _, fn := range fns {
 		if err := fn(follow); err != nil {
@@ -58,8 +70,10 @@ func runFollowValFns(follow *domain.Follow, fns ...followValFn) error {
 	return nil
 }
 
+// A followValFn is any function that takes in a pointer to a domain.Follow object and returns an error.
 type followValFn func(follow *domain.Follow) error
 
+// followExists makes sure that the Follow record to be deleted actually exists.
 func (fv *followValidator) followExists(follow *domain.Follow) error {
 	err := fv.db.First(follow, follow).Error
 	if err != nil {
@@ -72,6 +86,7 @@ func (fv *followValidator) followExists(follow *domain.Follow) error {
 	return nil
 }
 
+// followedIsNotFollower makes sure that the followed user and the follower user are not the same person.
 func (fv *followValidator) followedIsNotFollower(follow *domain.Follow) error {
 	if follow.FollowerID == follow.FollowedID {
 		return errs.Errorf(errs.EINVALID, "You cannot follow yourself.")
@@ -79,6 +94,7 @@ func (fv *followValidator) followedIsNotFollower(follow *domain.Follow) error {
 	return nil
 }
 
+// followedUserExists makes sure that the user to be followed actually exists.
 func (fv *followValidator) followedUserExists(follow *domain.Follow) error {
 	err := fv.db.First(&domain.User{ID: follow.FollowedID}).Error
 	if err != nil {
@@ -91,6 +107,7 @@ func (fv *followValidator) followedUserExists(follow *domain.Follow) error {
 	return nil
 }
 
+// notAlreadyFollowed makes sure that the follower user isn't already following the followed user.
 func (fv *followValidator) notAlreadyFollowed(follow *domain.Follow) error {
 	err := fv.db.First(follow, follow).Error
 	if err == nil {
@@ -101,15 +118,19 @@ func (fv *followValidator) notAlreadyFollowed(follow *domain.Follow) error {
 	return nil
 }
 
+// Create stores the data from the Follow object as a new database record.
+// On success, it eager-loads (preloads) the follower and followed user relations,
+// so that the json response displays the full user data of each.
 func (fg *followGorm) Create(follow *domain.Follow) error {
 	err := fg.db.Create(follow).Error
-	fg.db.Preload("Followed").Preload("Follower").First(follow)
 	if err != nil {
 		return err
 	}
+	fg.db.Preload("Followed").Preload("Follower").First(follow)
 	return nil
 }
 
+// Delete permanently deletes the database record matching the data from the Follow object.
 func (fg *followGorm) Delete(follow *domain.Follow) error {
 	err := fg.db.Delete(follow).Error
 	if err != nil {
